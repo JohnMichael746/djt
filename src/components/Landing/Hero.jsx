6d2d58695937fc56e5a9aad9b0cdca394231e140
johnmichael <withyou746@gmail.com>
2024-06-03 14:58:48 -0400

    flag

--- src/components/Landing/Hero.jsx
+++ src/components/Landing/Hero.jsx
@@ -3,7 +3,7 @@ import { CommunityIcon, Curve, LinkIcon2 } from "../../assets/constants";
 const Hero = () => {
   return (
     <section className="relative" id="home">
-      <div className="max-w-6xl px-4 mx-auto bg-[url(flag.jpg)] bg-no-repeat bg-cover">
+      <div className="max-w-6xl px-4 mx-auto bg-[url(/flag.jpg)] bg-no-repeat bg-cover">
         <div className="flex flex-col items-center pt-32 max-md:flex-col-reverse md:flex-row sm:pt-40 md:pb-10 lg:pb-0">
           <div className="w-[90%] sm:w-[75%] md:w-[55%] md:-mt-11">
             <h1 className="font-coiny text-[#df1731] text-[35px] font-medium text-center">WELCOME TO</h1>
